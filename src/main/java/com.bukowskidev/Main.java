671e77e80271c7c52a780203c161babd4c9dc1cc
Bukowski <bukowski.przemyslaw.1988@gmail.com>
2023-05-01 14:16:24 +0200

    Refactoring repeting loops

--- src/main/java/com.bukowskidev/Main.java
+++ src/main/java/com.bukowskidev/Main.java
@@ -8,45 +8,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
-        int loan = 0;
-        float annualInterest = 0;
-        byte years = 0;
-
         System.out.println("Witaj w kalkulatorze kredytowym");
         System.out.println("-------------------------------");
 
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("Podaj kwotę kredytu:");
-            loan = scanner.nextInt();
-            if (loan >= 1000 && loan <= 5_000_000)
-                break;
-            System.out.println("Kwota kredytu powinna wynosić od tysiąca do 5 milionów zł");
-        }
-
-        while (true) {
-            System.out.println("Podaj roczne oprocentowanie:");
-            annualInterest = scanner.nextFloat();
-            if (annualInterest >= 1 && annualInterest <= 50)
-                break;
-            System.out.println("Oprocentowanie powinno być w przedziale od 1 do 50");
-        }
-
-        while (true) {
-            System.out.println("Podaj okres spłaty (ilość lat): ");
-            years = scanner.nextByte();
-            if (years >= 1 && years <= 40)
-                break;
-            System.out.println("Okres kredytowania powinnien mieścić się w zakresie od roku do 40 lat");
-        }
+        int loan = (int) readNumber("Kredyt: ", 1000, 1000000);
+        float annualInterest = (float) readNumber("Oprocentowanie: ", 1, 50);
+        byte years = (byte) readNumber("Okres kredytowania", 1, 40);
 
          double mortage = calculateMortgage(loan, annualInterest, years);
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortage);
         System.out.println("Miesięczna rata kredytu wynosi : " + mortgageFormatted);
+    }
+
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.println(prompt);
+            value = scanner.nextInt();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Podaj wartość między" + min + " i " + max );
+        }
+        return value;
     }
 
     public static double calculateMortgage(int loan, float annualInterest, byte years) {
